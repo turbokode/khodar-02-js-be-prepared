b2886f302896dd77d99f9d4f52eee2cae4888175
Luis Saiete <luistsaiete@gmail.com>
2024-07-26 11:20:58 +0200

    Finish dashboard page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import logo from '../../assets/logo.svg';
-import { OctagonAlert } from 'lucide-react';
+import { OctagonAlert, Bell, ChevronRight } from 'lucide-react';
 import './styles.css';
 import { MetricsCard } from '../../components/MetricsCard';
 import { ButtonWithIcon } from '../../components/ButtonWithIcon';
@@ -65,6 +65,52 @@ export function Home() {
             </tbody>
           </table>
         </main>
+
+        <aside>
+          <header>
+            <Bell size={24} color="#000" />
+            <h2>Notificações</h2>
+            <span>4</span>
+          </header>
+          <ul>
+            <li>
+              <section>
+                <div className="circle full"></div>
+                <span>846383748</span> |{' '}
+                <span className="message">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos ratione ullam suscipit similique odit
+                  consectetur voluptatibus inventore libero voluptatum est, quidem, quam commodi vel laboriosam magni
+                  tempora quia officiis aperiam?
+                </span>
+              </section>
+              <ChevronRight size={12} color="#000" />
+            </li>
+            <li>
+              <section>
+                <div className="circle full"></div>
+                <span>846383748</span> |{' '}
+                <span className="message">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos ratione ullam suscipit similique odit
+                  consectetur voluptatibus inventore libero voluptatum est, quidem, quam commodi vel laboriosam magni
+                  tempora quia officiis aperiam?
+                </span>
+              </section>
+              <ChevronRight size={12} color="#000" />
+            </li>
+            <li>
+              <section>
+                <div className="circle"></div>
+                <span>846383748</span> |{' '}
+                <span className="message">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos ratione ullam suscipit similique odit
+                  consectetur voluptatibus inventore libero voluptatum est, quidem, quam commodi vel laboriosam magni
+                  tempora quia officiis aperiam?
+                </span>
+              </section>
+              <ChevronRight size={12} color="#000" />
+            </li>
+          </ul>
+        </aside>
       </div>
     </div>
   );
